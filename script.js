aa46cf9512dbac61023397d21bb6eb3a981d6092
YashasviGh <130542235+YashasviGh@users.noreply.github.com>
2025-03-24 14:44:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ const pixelId = '1254725762274850';
 const accessToken = 'EAAMyE4ynYOIBO2fUML9YgUICMGr2u8sZByAg6RlqK0R6blEshtv9aZCgWcFCNFcaRXkm0ddTDtW9VFNhfWTiAMM3ZA2ucZCuDXAqouHgdCmWAk13o18ZBpU6Lce79DWEdQUCZBfa2ul6jLNpbQR8ALMA6XiirsBt6piV5ZAQ95gB2JMfrCo04EmTqwkqZAmO9AkySCAvm9ks9riNbZAZBen58ZD';
 
 // Event data
+const pageViewEvent = {
+  'event_name': 'PageView',
+  'event_time': Date.now(),
+  'user_data': {
+    'client_user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'client_ip_address': '192.168.1.1',
+  },
+};
+const addToCartEvent = {
+  'event_name': 'AddToCart',
+  'event_time': Date.now(),
+  'user_data': {
+    'client_user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'client_ip_address': '192.168.1.1',
+  },
+  'custom_data': {
+    'currency': 'USD',
+    'value': 5.99,
+  },
+};
 const eventData = {
   'event_name': 'Purchase',
   'event_time': Date.now(),
